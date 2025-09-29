23dc2da7d557acb9274455d5753c2f2c4b6aedf4
Aidan Laceda <mralaceda@gmail.com>
2025-09-29 12:29:50 +0100

    Test commit

--- podiumPredict.py
+++ podiumPredict.py
@@ -8,12 +8,25 @@ Lifter: website + "u/" + lifter
 Lifter stats: website + "api/liftercsv/" + lifter
 '''
 
-#lifter = input("Enter lifter's name: ").replace(" ", "").lower()
-temp = "adamduane"
-lifter_stats = op + "api/liftercsv/" + temp
 
+def liftingcast_read():
+    
 
+def op_read(lifter: str):
+    '''
+        Searches for the lifter on OpenPowerlifting. Two outcomes: (1) The lifter exists on the OpenPowerlifting
+        database (2) First time lifter, in which case they will be assigned a "WILDCARD" status.
+    '''
+    lifter = lifter.replace(" ", "").lower()
+    lifter_stats = op + "api/liftercsv/" + lifter   #URL for lifter's CSV
+    try:
+        lifterDF = pd.read_csv(lifter_stats)    #locate lifter on OpenPowerlifting
+    except:
+        pass    #Requires implementation
 
-lifterDF = pd.read_csv(lifter_stats)
-lifterDF = lifterDF[["Best3SquatKg", "Best3BenchKg", "Best3DeadliftKg", "TotalKg"]]
-print(lifterDF)
+    finally:
+        lifterDF = lifterDF[["Best3SquatKg", "Best3BenchKg", "Best3DeadliftKg", "TotalKg"]]
+        print(lifterDF)
+
+if __name__ == "__main__": 
+    op_read("aidanlaceda")
